6ff30f07b7bd413af9c9eac098d3a97c217e1af4
larsbirger <larsbirger.bergmaal@gmail.com>
2025-10-01 21:49:17 +0200

    Update Matriser.java
    det er det som trengs etter oppgaven

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,33 +4,62 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		System.out.println(tilStreng(matrise));
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String streng = "";
 		
+		for ( int i = 0; i < matrise.length; i++) {
+			for ( int j = 0; j < matrise[i].length; j++) {
+				streng += matrise[i][j] + " ";
+			}
+			streng += "\n";
+		}
+
+		return streng;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		//anntar at alle rekkene i matrisen er av samme lengde...
+		int iMax = matrise.length;
+		int jMax = matrise[0].length;
+
+		int[][] m = new int[iMax][jMax];
+
+		for ( int i = 0; i < iMax; i++) {
+			for ( int j = 0; j < jMax; j++) {
+				m[i][j] = tall * matrise[i][j];
+			}
+		}
+
+		return m;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
 		
+		if (a.length != b.length) {
+			return false;
+		}
+
+		if (a[0].length != b[0].length) {
+			return false;
+		}
+
+
+		for ( int i = 0; i < a.length; i++) {
+			for ( int j = 0; j < a[0].length; j++) {
+				if ( a[i][j] != b[i][j]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
 	// e)
